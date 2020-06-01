8a5496fcc539a03a7daa69aa726332d8aa18dd01
HannHank <hanke.johann@gmail.com>
2020-06-01 15:40:15 +0200

    fixed issue with battery - added attribute callback for batter and mode

--- main.py
+++ main.py
@@ -1,6 +1,46 @@
 from dronekit import connect
-
+import time
+# diffrent modes:
+    # 5: Loiter
+    # 6: RTL
+    # 7: Circle
+    # 9: Land
 # Connect to the Vehicle (in this case a UDP endpoint)
-vehicle = connect('/dev/ttyAMA0', wait_ready=True, baud=57600)
+vehicle = connect('/dev/ttyUSB0', wait_ready=True, baud=57600)
 
-print("Mode: {0}".format(vehicle.mode.name))
+#call for vehicle mode
+@vehicle.on_attribute('mode')
+def mode_callback(self,attr,msg):
+    print("Mode Callback: ",msg)
+
+@vehicle.on_attribute('battery')
+def mode_callback(self,attr,msg):
+    print("Battery Callback: ",msg)
+
+try:
+    while True:
+     #   vehicle.mode
+        # getting system status
+        # action = input("1:Change Mode \n 2:Incomming")
+        # if(int(action) == 1):
+        #     action = input("change Mode to: \n 0: stabilize 5: Loiter")
+        #     vehicle.mode = int(action)
+         
+        # print("System: ".format(vehicle.system_status))
+        
+        # # getting mode
+        # print("Mode: {0}".format(vehicle.mode.name))
+
+        # # getting the attitude
+        # print("vehicle attitude: {0}".format(vehicle.attitude)) 
+
+        # getting the battery
+        print("Battery: {0}".format(vehicle.battery))
+       # print("hello".format(vehicle._voltage))
+
+       # vehicle.add_attribute_listener('mode',mode_callback)
+     
+        time.sleep(10)
+except KeyboardInterrupt:
+    print("exited the program")
+    vehicle.close()
